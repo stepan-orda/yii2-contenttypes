99c0f2edfd98d964694be4b4a4e6d0f668eb1147
Stepan <stepanorda87@gmail.com>
2015-05-05 16:58:34 +0300

    test

---
+++ ModelTrait.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Stepan Orda
+ * @author Stepan Orda <stepanorda87@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+
+
+namespace stepanorda\contenttypes;
+
+trait ModelTrait
+{
+    public function getModel()
+    {
+
+    }
+}
